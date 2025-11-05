6cdc96f0e0b12c040296ea8175c962bd6df4afed
Tim Sullivan <timothyj.sullivan1@gmail.com>
2025-11-05 22:09:38 +0000

    set up terraform settings to ignore the gpt model validation error

---
+++ infra/ignore_validation.tf
@@ -0,0 +1,15 @@
+resource "azurerm_cognitive_deployment" "gpt" {
+  cognitive_account_id       = azurerm_ai_services.ai_hub.id
+  name                       = var.openai_deployment_name
+
+  model {
+    format  = "OpenAI"
+    name    = var.openai_model_name
+    version = var.openai_model_version
+  }
+  
+  sku {
+    capacity = 150
+    name     = "GlobalStandard"
+  }
+}
